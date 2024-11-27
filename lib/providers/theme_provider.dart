106b67677a96b0e2935687ddbb7bdd911d881655
Diaz Jonatan E <diaz.jonatane@gmail.com>
2024-11-27 11:18:53 -0300

    fix errores

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -13,41 +13,14 @@ class ThemeProvider extends ChangeNotifier {
   bool get isDarkMode => _isDarkMode;
 
   void setLight() {
-    print('setLight');
     _isDarkMode = false;
     temaActual = DefaultTheme.lightTheme;
     notifyListeners();
   }
 
   void setDark() {
-    print('setDark');
     _isDarkMode = true;
     temaActual = DefaultTheme.darkTheme;
     notifyListeners();
   }
 }
-
-
-
-/*import 'package:flutter/material.dart';
-import '../themes/default_theme.dart';
-
-class ThemeProvider extends ChangeNotifier {
-  ThemeData temaActual;
-
-  ThemeProvider({required bool isDarkMode})
-      : temaActual =
-            (isDarkMode) ? DefaultTheme.darkTheme : DefaultTheme.lightTheme;
-
-  setLight() {
-    print('setLight');
-    temaActual = DefaultTheme.lightTheme;
-    notifyListeners();
-  }
-
-  setDark() {
-    print('setLight');
-    temaActual = DefaultTheme.darkTheme;
-    notifyListeners();
-  }
-}*/
