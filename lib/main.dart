5e212b13564cc9170a70a9bb3506c096a76dfdae
NicoValenciano <lordmacako@gmail.com>
2024-11-28 20:18:57 -0300

    nueva pantalla de perfil

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,8 @@ class MyApp extends StatelessWidget {
       routes: {
         'home': (context) => const HomeScreen(), // Pantalla principal
         'profile': (context) => const ProfileScreen(), // Pantalla de perfil
+        'custom_list_item': (context) =>
+            const CustomListItem(), // Pantalla de perfil
       },
     );
   }
